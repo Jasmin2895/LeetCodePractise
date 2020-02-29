d4d5a4d37375f9daa9db7d45f6b8c2666307d1f6
buckleader <jasmin28virdi@gmail.com>
2020-02-29 16:14:59 +0530

    basic tricky questions

--- JavascriptInterviewQuestions/InterviewQuestions.js
+++ JavascriptInterviewQuestions/InterviewQuestions.js
@@ -17,4 +17,42 @@ var output = (function () {
 })();
 // ans is undefined
 
+console.log(true + 0);  // ans is 0
 
+var test = function () {
+
+    for (var i = 1; i < 5; i++) {
+
+        setTimeout(() => console.log(i), 1000)  // 5 5 5 5
+
+    }
+
+}
+
+//output will be 4 that will be printed 5 times
+
+function foo1() {
+
+    return {
+
+        bar: "hello"
+
+    };
+
+}
+
+// output will be {bar:hello}
+
+
+function foo2() {
+
+    return
+
+    {
+
+        bar: "hello"
+
+    };
+
+}
+//output will be undefined
